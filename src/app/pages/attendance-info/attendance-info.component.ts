de20a7bfa42feebb042a57dbc0679873f543a2f2
Nikhil Ade <nickade7@gmail.com>
2025-07-02 18:33:52 +0530

    bug fix

--- src/app/pages/attendance-info/attendance-info.component.ts
+++ src/app/pages/attendance-info/attendance-info.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-attendance-info',
   standalone: true,
-  imports: [],
+  imports: [
+
+   
+ 
+ 
+  ReactiveFormsModule,
+  FormsModule,
+
+  
+  ],
   templateUrl: './attendance-info.component.html',
   styleUrl: './attendance-info.component.css'
 })
 export class AttendanceInfoComponent {
-
+ attendanceData = [
+   
+    {
+      name: 'Nikhil Ade',
+      date: '2025-07-01',
+      status: 'Present',
+      inTime: '09:05 AM',
+      outTime: '06:10 PM'
+    },
+    {
+      name: 'Samtha Kale',
+      date: '2025-07-01',
+      status: 'Absent',
+      inTime: '-',
+      outTime: '-'
+    },
+    {
+      name: 'Damini Patil',
+      date: '2025-07-01',
+      status: 'Leave',
+      inTime: '-',
+      outTime: '-'
+    }
+  ];
 }
